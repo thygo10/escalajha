2648b33cb8aa3ef938b2d66bffd73821ca6d89e0
Thiago Farias <thygo10@icloud.com>
2026-07-25 01:12:40 -0300

    feat: initialize production and development environment configuration files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://SEU_PROJETO.supabase.co',
-  supabaseAnonKey: 'SUA_CHAVE_ANON_SUPABASE',
+  supabaseUrl: 'https://rjcvjuymsvatabmemncf.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqY3ZqdXltc3ZhdGFibWVtbmNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5NDM2NjAsImV4cCI6MjEwMDUxOTY2MH0.1X4R7B0pEA_EGKamLYozrd3p7bsGwU2GsK8sVGyDsVA',
   demoMode: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://SEU_PROJETO.supabase.co',
-  supabaseAnonKey: 'SUA_CHAVE_ANON_SUPABASE',
-  demoMode: true,
+  supabaseUrl: 'https://rjcvjuymsvatabmemncf.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqY3ZqdXltc3ZhdGFibWVtbmNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5NDM2NjAsImV4cCI6MjEwMDUxOTY2MH0.1X4R7B0pEA_EGKamLYozrd3p7bsGwU2GsK8sVGyDsVA',
+  demoMode: false,
   demoEmail: 'demo@joaohenrique.com',
   demoPassword: 'DEMO_2026_JH'
 };
